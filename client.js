5f89bd902fe0766b838f7de22cda3f2a2671dd06
RF-nci <x25130901@student.ncirl.ie>
2026-02-20 14:20:31 +0000

    Commit test

--- client.js
+++ client.js
@@ -2,10 +2,8 @@ import { io } from "socket.io-client";
 
 const socket = io("http://localhost:3000");
 
-// Send a message to the server
 socket.emit("message", "Hello Server!");
 
-// Listen for responses from the server
 socket.on("response", (data) => {
 console.log(data);
 });
